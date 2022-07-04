496b529f4aa0509be160897528f01ac913238fe5
A-RandomGithubUser101 <96635244+A-RandomGithubUser101@users.noreply.github.com>
2022-07-04 19:24:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project");
+
+var firstname = "Ranbir";
+var lastname=Kapoor
+var join=firstname+lastname;
+console.log(join);
+
+var variable1 = 10
+
+var variable2 = 5
+
+var add=variable1+variable2
+console.log(add);
+
+var subtract=variable1-variable2
+console.log(subtract);
+
+var multiply=variable1*variable2
+console.log(multiply);
+
+var divide=variable1/variable2
+console.log(divide);
